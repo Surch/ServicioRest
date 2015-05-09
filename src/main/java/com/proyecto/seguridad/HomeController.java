061f46af3b8fc08f29d07ea8eecd16fdfe6f8c31
Surch <sergioromero@opendeusto.es>
2015-05-09 16:45:31 +0200

    parece que no se ha colgado esto

--- src/main/java/com/proyecto/seguridad/HomeController.java
+++ src/main/java/com/proyecto/seguridad/HomeController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -36,4 +38,22 @@ public class HomeController {
 		return "home";
 	}
 	
+	/**
+	 * Responde a la URL "/persona" gracias al RequestMapping.
+	 * Requiere de un parametro llamado "id", podría ser obcional pero así es más sencillo.
+	 * En vez de devolver un String como el nombre de la vista home.jsp va a devolver un objeto del tipo Persona.
+	 * La etiqueta @ResponseBody le dice a Spring Framework que convierta en objeto una respuesta http.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value="/persona", method = RequestMethod.GET)
+	public @ResponseBody Persona obtenerPersona(@RequestParam(value="id", required=true) int id){
+		//Logica para obtener los datos de la persona
+		Persona p = new Persona();
+		p.setNombre("Sergio");
+		p.setApellido("Romero");
+		p.setEdad(20);
+		return p;
+	}
 }
